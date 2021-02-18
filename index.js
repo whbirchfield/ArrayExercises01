ad58f8efb1e7c8e0f30e00219af02ab83aea7b77
Will B <willhbjr@gmail.com>
2021-02-18 05:05:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Create an array with the following entry: 273.15.
+let practiceFile = [273.15];
+
+//Use the push method to add the number 42 and the string "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+console.log(practiceFile);
+practiceFile.push(42);
+practiceFile.push("hello");
+
+console.log(practiceFile);
+//Use a single push to add the items false, -4.6, and “87” to the array.  Print the array to confirm the changes.
+
+practiceFile.push("false", -4.6, "87");
+console.log(practiceFile);
